7cbb584fc160fcbbee8547720df43f39d86b2ad2
Aarohi Srivastava <aarohi251101@gmail.com>
2022-06-13 11:15:08 +0530

    ..

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-    api_url: "http://localhost:5000",
+    // api_url: "http://localhost:5000",
+    api_url: "https://bookwormsrestapi.herokuapp.com",
   };
   
   export default app_config;
